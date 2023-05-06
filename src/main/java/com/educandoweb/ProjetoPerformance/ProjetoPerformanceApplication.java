fa065c7be1dfbaf756d74d49436460f365a3f9b6
jvroot <joaovlima.delima@gmail.com>
2023-05-06 19:37:50 -0300

    nova branch

---
+++ src/main/java/com/educandoweb/ProjetoPerformance/ProjetoPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.ProjetoPerformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/ProjetoPerformance/ProjetoPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.ProjetoPerformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
